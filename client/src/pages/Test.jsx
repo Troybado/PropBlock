60befd03360d304214a7a92e409fb3eb5b0e4f3f
qusai <zazi.zizo2216@gmail.com>
2022-12-07 07:40:55 +0400

    added 4 properties

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,67 +1,293 @@
 import { Upload } from "@web3uikit/core";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import React from "react";
-import key from "ipfs-api/src/key";
+//import ipfskey from "ipfs-api/src/key";
+import { type } from "os";
+import { WriteStream } from "fs";
+//import ipfs from "../modules/ipfs";
+
 
 const nacl = require("tweetnacl");
 nacl.util = require("tweetnacl-util");
 var { Crypt } = require("hybrid-crypto-js");
-const console = require("console-browserify");
+
+//Result of openSSL
+const publicKey =
+  "-----BEGIN PUBLIC KEY-----\n" +
+  "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4e3R+O5jcNragqe05vmQmFKzqaGMm25z9moHIhIBv8Fj0SwFF1ZRoA1WeL4PZ1xMDLirn1rR7tcoj5oNKtjd671j4SW7AV0NTv9y76dJlIaYTwyefFIvX40OwULId1xYr55XN2xN85rrQxtRHj5I5AeVDFr+attODqmKSQ3tVSQIDAQAB" +
+  "-----END PUBLIC KEY-----";
+const privateKey =
+  "-----BEGIN PRIVATE KEY-----\n" +
+  "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALh7dH47mNw2tqCp7Tm+ZCYUrOpoYybbnP2agciEgG/wWPRLAUXVlGgDVZ4vg9nXEwMuKufWtHu1yiPmg0q2N3rvWPhJbsBXQ1O/3Lvp0mUhphPDJ58Ui9fjQ7BQsh3XFivnlc3bE3zmutDG1EePkjkB5UMWv5q204OqYpJDe1VJAgMBAAECgYAqzFlGP8VCsV7E/ycN0mGhhAJpjzYRebl+DC43uqWhUn0Qj6YH8XiweLWQMS3Bh+fdQQSrLGBKou32Ti//US6lbZ1k/XVA8b+c41511iVCs+Ez9+8g7213oxbVZTfgpZr/bMOdy6KWQj11h72KSX3SmsnUEYOejGAidKgJEtfL0QJBAOKke8c4mcXlhGz8fqmPjn4ap6F2DwbpkQjGovH5QN51bS7RfjQS+w2KywuusxsuooL+gPH7FATi9bZwU/oah08CQQDQYO15nsXvtyPMdHU1kUmnllX0P0ECAlWpR+eEnn9lZ7+8a3/0lWNB2vZa2df00y+1UD+yRtlEYdupjJT8YbPnAkEAhPZaZ5YdIKx19DptVKyTvAeHMIJCbpu4Kp3eGndQRzI8y6q7j3FTN66lUEU9bkT9sjv9Cfly1SsRW+aqXaASoQJBAIF8yfU6obt9rcQ+VhuMhh2bARy/NdFezAZehVLW/0lveClJEAFX148z1sTL/DPKDCp6jKpm9lpVCSBKGMbgm98CQGiZ8uTrc/7ObWSvSCgBNBbeXFEB97U4ZwXvrDccqeEPL4RhkDNRHuIBeVvc9wXW9lSPeRvcrUQNY6b+hxXAC+0=" +
+  "-----END PRIVATE KEY-----";
 
 const Test = () => {
-  var crypt = new Crypt({ md: "sha1" });
+  const ref = useRef(null);
   const [frontIdDocument, setFrontIdDocument] = useState({});
+  const [encryptedFile, setencryptedFile] = useState();
+  const [decryptedFile, setdecryptedFile] = useState();
+  const [ciphertext, setciphertext] = useState();
 
-  //START OF RSA
+  const NodeRSA = require("node-rsa");
+  const FileSaver = require("file-saver");
 
-  //Result of openSSL
-  const publicKey =
-    "-----BEGIN PUBLIC KEY-----\n" +
-    "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4e3R+O5jcNragqe05vmQmFKzqaGMm25z9moHIhIBv8Fj0SwFF1ZRoA1WeL4PZ1xMDLirn1rR7tcoj5oNKtjd671j4SW7AV0NTv9y76dJlIaYTwyefFIvX40OwULId1xYr55XN2xN85rrQxtRHj5I5AeVDFr+attODqmKSQ3tVSQIDAQAB" +
-    "-----END PUBLIC KEY-----";
-  const privateKey =
-    "-----BEGIN PRIVATE KEY-----\n" +
-    "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALh7dH47mNw2tqCp7Tm+ZCYUrOpoYybbnP2agciEgG/wWPRLAUXVlGgDVZ4vg9nXEwMuKufWtHu1yiPmg0q2N3rvWPhJbsBXQ1O/3Lvp0mUhphPDJ58Ui9fjQ7BQsh3XFivnlc3bE3zmutDG1EePkjkB5UMWv5q204OqYpJDe1VJAgMBAAECgYAqzFlGP8VCsV7E/ycN0mGhhAJpjzYRebl+DC43uqWhUn0Qj6YH8XiweLWQMS3Bh+fdQQSrLGBKou32Ti//US6lbZ1k/XVA8b+c41511iVCs+Ez9+8g7213oxbVZTfgpZr/bMOdy6KWQj11h72KSX3SmsnUEYOejGAidKgJEtfL0QJBAOKke8c4mcXlhGz8fqmPjn4ap6F2DwbpkQjGovH5QN51bS7RfjQS+w2KywuusxsuooL+gPH7FATi9bZwU/oah08CQQDQYO15nsXvtyPMdHU1kUmnllX0P0ECAlWpR+eEnn9lZ7+8a3/0lWNB2vZa2df00y+1UD+yRtlEYdupjJT8YbPnAkEAhPZaZ5YdIKx19DptVKyTvAeHMIJCbpu4Kp3eGndQRzI8y6q7j3FTN66lUEU9bkT9sjv9Cfly1SsRW+aqXaASoQJBAIF8yfU6obt9rcQ+VhuMhh2bARy/NdFezAZehVLW/0lveClJEAFX148z1sTL/DPKDCp6jKpm9lpVCSBKGMbgm98CQGiZ8uTrc/7ObWSvSCgBNBbeXFEB97U4ZwXvrDccqeEPL4RhkDNRHuIBeVvc9wXW9lSPeRvcrUQNY6b+hxXAC+0=" +
-    "-----END PRIVATE KEY-----";
 
-  const myEncrypt = (data) => {
-    try {
-      //generate a signature to sign the data
-      var signature = crypt.signature(privateKey, data);
-      console.log("----SIGNATURE----\n", signature);
+  const pubkey = new NodeRSA(publicKey);
+  const privkey = new NodeRSA(privateKey);
 
-      //perform encryption
-      var encrypted = crypt.encrypt(publicKey, data, signature); //signs and encrypts the encrypted with the generated signature and pubkey
-      console.log("----ENCRYPTED----\n", encrypted);
 
-      //perform decryption
-      var decrypted = crypt.decrypt(privateKey, encrypted); //decrypts with privkey
-      console.log("----DECRYPTED----\n", decrypted);
+  async function readContent(event) {
 
-      //decrypts the signature of the result using the pubkey and compares the obtained hash from the signature to the original hash, returns true if sigs are equal
-      var verify = crypt.verify(publicKey, decrypted.signature, data);
-      console.log("----VERIFICATION----\n", verify);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }
+
+
+  useEffect(() => {
+    console.log("encrypted file changed", encryptedFile)
+  }, [encryptedFile])
+
+
+
+   // var encryptedfile = new File([file], file.name, {type: file.type});
+   // const blobtoencrypt = new Blob([file], {type: file.type});
+
+  
+
+  const downloadDoc = (file) => {
+    console.log("Download Set!");
+    const a = document.querySelector("#link");
+
+    a.setAttribute("download", file.name );
+  }
+
+
+  const previewFile = (myfile) => {
+    const preview = document.querySelector('#specific');
+    const reader44 = new FileReader();
+  
+    reader44.addEventListener("load", () => {
+      // convert image file to base64 string
+
+      console.log("original ", myfile);
+
+      preview.src = reader44.result;
+    }, false);
+  
+      reader44.readAsDataURL(myfile);
+    
+  }
+
+  const downloadEncryption = (myfile) => {
+    const reader44 = new FileReader();
+  
+    reader44.addEventListener("load", () => {
+      console.log(reader44.result);
+
+      const encryptedtext = pubkey.encrypt(reader44.result, 'base64'); //encrypts the reader result. (reader44.result returns a URL representing the file's data)
+      setciphertext(encryptedtext); 
+
+      const a = document.querySelector("#encrypt");
+
+      //sets the type of the file to octet-stream which converts the file to a binary file and appends the encrypted text.
+      a.setAttribute('href', 'data:application/octet-stream,' + encryptedtext);
+
+      //change the extension and sets a download attribute to allow for user to download the encrypted file. 
+      a.setAttribute('download', myfile.name + '.encrypted'); 
+
+    }, false);
+  
+      reader44.readAsDataURL(myfile); //return a URL representing the file's data as a base64 encoded string
+  }
+  
+
+  
+
+  const downloadDecryption = (filetodecrypt) => { 
+    //encryptedfile is uploaded by the user because I have not found a way to set assemble the encryptedFile in the encryption function
+    const reader2 = new FileReader();
+
+    reader2.addEventListener("load", () => {
+
+      const decryptedtext = privkey.decrypt(ciphertext); //decrypts the cipher
+      const b = document.querySelector("#decrypt");
+
+      //because the plain text is the URL representing the original file's data, decrypting it will give us the URL of the original file data
+      b.setAttribute('href', decryptedtext); 
+
+      b.setAttribute('download', filetodecrypt.name.replace('.encrypted','')); //removing the .encrypted extension and downloading the decrypted file
+    }, false);
+
+    reader2.readAsText(filetodecrypt);
+
+  }
+
+
+ 
+
 
   return (
     <div>
-      <Upload value="" onChange={setFrontIdDocument} theme="withIcon" />
+      ENCRYPT
+      <Upload value="" onChange={setFrontIdDocument} theme="withIcon" id='myfile' />
       <h1>Upload Pic and Click on button</h1>
-      <button
+      <br />
+      <br />
+      {/* <button onClick={() => {}}>Decrypt</button> */}
+      {/* <p id="image" ref={ref}></p> */}
+      {/* <p id="image2" ref={ref}></p> */}
+      {/* <a href="" id="link">
+        Click to Download encrypted
+      </a>
+  <br />*/}
+       <button onClick={() => {downloadEncryption(frontIdDocument)}}>Click to launch encrypted download function</button> 
+       <a href="" id="encrypt">
+      Click to Download Encrypted</a><br/>
+
+       <br></br>
+      <textarea id="fileContents"></textarea><br></br>
+      <textarea id="encryptedContents"></textarea><br></br>
+      <textarea id="decryptedContents"></textarea><br></br>
+      <button onClick={() => {downloadDecryption(frontIdDocument)}}>Click to launch decrypted download function</button> 
+
+      <a href="" id="decrypt">
+      Click to Download Decrypted</a>
+      
+      <br/>
+      <br/><br/>
+
+      <button onClick={() => {previewFile(frontIdDocument)}} >Preview FILE</button><br />
+      <img src="" height="200" alt="Image preview" id="specific"/>
+
+
+
+
+      {/* { <button
         onClick={() => {
-          myEncrypt(frontIdDocument);
+          myDecrypt(encryptedd);
         }}
       >
-        Encrypt Me
-      </button>
+        Decrypt Me
+      </button> } */}
     </div>
   );
 };
 
 export default Test;
+
+//   const readFileDataAsBase64 = async(e) => {
+//     return new Promise(async (resolve, reject) => {
+//         const reader = new FileReader();
+
+//         reader.onload = (event) => {
+//             resolve(frontIdDocument);
+//         };
+
+//         reader.onerror = (err) => {
+//             reject(err);
+//         };
+
+//         reader.readAsDataURL(frontIdDocument);
+//     });
+// }
+
+//   const e = async () => {
+//     console.log(readFileDataAsBase64(frontIdDocument))
+//   }
+
+//   function readDoc(file) {
+//     //const preview = document.querySelector('#image');
+//     const preview2 = document.querySelector("#image2");
+//     const el2 = ref.current;
+//     const a = document.querySelector("#link");
+//     const b = document.querySelector("#link2");
+
+//     console.log(frontIdDocument)
+//     //var fileBuffer = Buffer.from(frontIdDocument)
+//     //console.log(fileBuffer);
+
+//     //reading file content
+//     reader.addEventListener(
+//       "load",
+//       (event) => {
+//         console.log(frontIdDocument)
+//         var encrypted = crypt.encrypt(publicKey, reader.result); //encrypts the encrypted with the generated signature and pubkey
+//         a.setAttribute("href", "data:application/octet-stream," + encrypted);
+//         a.setAttribute("download", file.name + ".encrypted");
+
+//         var clonedFile = new File([file], file.name, {type: file.type});
+//         console.log(encrypted);
+//       },
+//       false
+//     );
+//     console.log("ENCRYPTED")
+
+//     reader.readAsDataURL(file);
+
+//     /*METADATA OF FILE*/
+//     const name = file.name ? file.name : "NOT SUPPORTED";
+//     const type = file.type ? file.type : "NOT SUPPORTED";
+//     const size = file.size ? file.size : "NOT SUPPORTED";
+
+//     //var decryptedFile = new File([file], decrypted, {type: file.type});
+//   }
+
+//   function decrypt(file) {
+//     const b = document.querySelector("#link2");
+
+//     //reading file content
+//     reader.addEventListener(
+//       "load",
+//       (event) => {
+//         var decrypted = crypt.decrypt(privateKey, reader.result);
+//         b.setAttribute("href", decrypted);
+//         b.setAttribute("download", file.name.replace(".encrypted", ""));
+
+//       },
+//       false
+//       );
+
+//       reader.readAsText(file);
+
+//     };
+
+//   //START OF RSA
+
+//   const myEncrypt = (data) => {
+//     try {
+//       console.log(data);
+
+//       //perform encryption
+//       var encrypted = crypt.encrypt(publicKey, data); //signs and encrypts the encrypted with the generated signature and pubkey
+//       setEncrypted(encrypted);
+//       console.log("----ENCRYPTED----\n", encrypted, typeof encrypted);
+
+//       var ciphered = JSON.parse(encrypted).cipher;
+//       console.log("\n\n", ciphered);
+
+//       //perform decryption
+//       var decrypted = crypt.decrypt(privateKey, encrypted); //decrypts with privkey
+//       console.log("----DECRYPTED----\n", decrypted);
+
+//       /*// //decrypts the signature of the result using the pubkey and compares the obtained hash from the signature to the original hash, returns true if sigs are equal
+//       // var verify = crypt.verify(publicKey, decrypted.signature, data);
+//       // console.log("----VERIFICATION----\n", verify);
+//             // //generate a signature to sign the data
+//       // var signature = crypt.signature(privateKey, data);
+//       // console.log("----SIGNATURE----\n", signature);*/
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const myDecrypt = (cipher) => {
+//     try {
+//       //perform decryption
+//       var decrypted = crypt.decrypt(privateKey, cipher); //decrypts with privkey
+
+//       console.log("----DECRYPTED----\n", decrypted);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
 /* Assigning every task in a function
 
@@ -244,15 +470,4 @@ const signData = (text) => {
 //     type: "pkcs1",
 //     format: "pem",
 //   });
-//   fs.writeFileSync("public.pem", exportedPublicKeyBuffer, {
-//     encoding: "utf-8",
-//   });
 
-//   // To export the private key and write it to file to use it in API
-//   const exportedPrivateKeyBuffer = keys.privKey.export({
-//     type: "pkcs1",
-//     format: "pem",
-//   });
-//   fs.writeFileSync("private.pem", exportedPrivateKeyBuffer, {
-//     encoding: "utf-8",
-//   });
